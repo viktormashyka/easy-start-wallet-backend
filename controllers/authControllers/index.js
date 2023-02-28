6217305fc0504d52bd0cf88399f18618d66e7007
Ruslan379 <arnov@i.ua>
2023-02-28 22:18:17 +0200

    refactoring authControllers

--- controllers/authControllers/index.js
+++ controllers/authControllers/index.js
@@ -2,10 +2,7 @@ const registrationController = require('./registrationController.js');
 const loginController = require('./loginController.js');
 const logoutController = require('./logoutController');
 const getCurrentController = require('./getCurrentController');
-const updatePatchUserSubscription = require('./updatePatchUserSubscription');
 const updateAvatar = require('./updateAvatar');
-const verifyEmail = require('./verifyEmail');
-const resendVerifyEmail = require('./resendVerifyEmail');
 const getBalance = require('./getBalance');
 const updateBalance = require('./updateBalance');
 const changeIsNotNewUser = require('./changeIsNotNewUser');
@@ -16,10 +13,7 @@ module.exports = {
   loginController,
   logoutController,
   getCurrentController,
-  updatePatchUserSubscription,
   updateAvatar,
-  verifyEmail,
-  resendVerifyEmail,
   getBalance,
   updateBalance,
   changeIsNotNewUser,
